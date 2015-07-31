6b9d7f9b5b00f854181ee173b3574f4c113bc49b
TuanAnh <anhdtse02636@gmail.com>
2015-07-31 20:55:29 +0700

    Update new transaction

---
+++ app/src/main/java/vn/lol/moneyhater/moneyhater/activity/NewTransactionActivity.java
@@ -0,0 +1,50 @@
+package vn.lol.moneyhater.moneyhater.activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import vn.lol.moneyhater.momeyhater.R;
+
+
+public class NewTransactionActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_transaction);
+        Button btAddAcc = (Button) findViewById(R.id.btAddNewTran);
+        btAddAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_transaction, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
